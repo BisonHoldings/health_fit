72101852003bfd0836ae9d3733af9b1f87aa5aca
neonankiti <yk.nanri@gmail.com>
2018-08-25 16:09:54 +0900

    create interfaces across platform - add some functions   - requestPermission   - disable   - data(WIP)

--- lib/health_fit.dart
+++ lib/health_fit.dart
@@ -10,4 +10,26 @@ class HealthFit {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  static Future<bool> get hasPermission async {
+    final bool hasPermission = await _channel.invokeMethod('hasPermission');
+    return hasPermission;
+  }
+
+  static Future<bool> get requestPermission async {
+    final bool requestPermission = await _channel.invokeMethod('requestPermission');
+    return requestPermission;
+  }
+
+  static Future<bool> get disable async {
+    final bool disabled = await _channel.invokeMethod('disable');
+    return disabled;
+  }
+
+  // FIXME add serializer and return to clients(dart) to the mapped objects
+  static Future<String> get data async {
+    final String data = await _channel.invokeMethod('getData');
+    return data;
+  }
+
 }
