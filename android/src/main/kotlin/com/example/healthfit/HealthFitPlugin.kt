4cde39e55729fe33f7ea144c86249bb82f32632e
neonankiti <yk.nanri@gmail.com>
2018-08-25 16:02:46 +0900

    make it compile

--- android/src/main/kotlin/com/example/healthfit/HealthFitPlugin.kt
+++ android/src/main/kotlin/com/example/healthfit/HealthFitPlugin.kt
@@ -9,6 +9,12 @@ import com.google.android.gms.fitness.FitnessOptions
 import com.google.android.gms.fitness.data.DataType
 import com.google.android.gms.fitness.request.DataReadRequest
 import java.util.concurrent.TimeUnit
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
 
 
 class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, ActivityResultListener {
@@ -32,29 +38,35 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
             "getPlatformVersion" -> {
             }
             "hasPermission" -> hasPermission(call, result)
+            "requestPermission" -> requestPermission(call, result)
             "disable" -> disable(result)
             "getData" -> {
-                getData(result)
+                // TODO add getData method with correct arguments
             }
             else -> result.notImplemented()
         }
     }
 
     private fun hasPermission(call: MethodCall, result: Result) {
-        val rawDataType = call.argument("dataType")
-        val rawPermission = call.argument("permission")
-        val dataType: DataType = when (rawDataType) {
-            "com.google.step_count.delta" -> DataType.TYPE_STEP_COUNT_DELTA
-            else -> throw IllegalArgumentException("$rawDataType is not allowed here.")
-        }
-        val permission: Int = when (rawPermission) {
-            0 -> FitnessOptions.ACCESS_READ
-            1 -> FitnessOptions.ACCESS_WRITE
-            else -> throw IllegalArgumentException("$rawPermission is not allowed here.")
-        }
+        val healthFitOption = getHealthFitOptions(call)
         val optionsBuilder = FitnessOptions.builder()
-        optionsBuilder.addDataType(dataType, permission)
+        optionsBuilder.addDataType(healthFitOption.dataType, healthFitOption.permission)
         result.success(GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(activity), optionsBuilder.build()))
+    }
+
+    private fun getHealthFitOptions(call: MethodCall): HealthFitOption =
+            HealthFitOption(getDataType(call), getPermission(call))
+
+
+    private fun getDataType(call: MethodCall): DataType = when (call.argument<String>("dataType")) {
+        "com.google.step_count.delta" -> DataType.TYPE_STEP_COUNT_DELTA
+        else -> throw IllegalArgumentException("${call.argument<String>("dataType")} is not allowed here.")
+    }
+
+    private fun getPermission(call: MethodCall): Int = when (call.argument<Int>("permission")) {
+        0 -> FitnessOptions.ACCESS_READ
+        1 -> FitnessOptions.ACCESS_WRITE
+        else -> throw IllegalArgumentException("${call.argument<Int>("permission")} is not allowed here.")
     }
 
     private fun disable(result: Result) {
@@ -65,14 +77,16 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
                 })
     }
 
-    // TODO make this FitnessOptions to android platform domain object
-    private fun requestPermission(result: Result, fitnessOptions: FitnessOptions) {
+    private fun requestPermission(call: MethodCall, result: Result) {
         deferredResult = result
+        val healthFitOption = getHealthFitOptions(call)
+        val optionsBuilder = FitnessOptions.builder()
+        optionsBuilder.addDataType(healthFitOption.dataType, healthFitOption.permission)
         GoogleSignIn.requestPermissions(
                 activity,
                 GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
                 GoogleSignIn.getLastSignedInAccount(activity),
-                fitnessOptions)
+                optionsBuilder.build())
     }
 
     private fun getData(result: Result, dataType: DataType, startAt: Long, endAt: Long, bucket: TimeUnit) {
@@ -96,25 +110,27 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
             .build()
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent): Boolean = when (resultCode) {
-        // TODO: put the concrete messages because requests and results have multiple status.
+    // TODO: put the concrete messages because requests and results have multiple status.
         Activity.RESULT_OK -> {
             when (requestCode) {
                 GOOGLE_FIT_PERMISSIONS_REQUEST_CODE -> {
-                    deferredResult.success(true)
+                    deferredResult?.success(true)
                     deferredResult = null
                     true
                 }
                 else -> {
-                    deferredResult.success(false)
+                    deferredResult?.success(false)
                     deferredResult = null
                     false
                 }
             }
         }
         else -> {
-            deferredResult.success(false)
+            deferredResult?.success(false)
             deferredResult = null
             false
         }
     }
 }
+
+data class HealthFitOption(val dataType: DataType, val permission: Int)
