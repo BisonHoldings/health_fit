3177a13bb3ccdab564d2c580e0a2b54ffd4e7da4
Naoki Kobayashi <knao124@gmail.com>
2018-08-25 13:22:41 +0900

    Show platform version, step count and weight in home view

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,21 +4,36 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:health_fit/health_fit.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HealthFit Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: _MyHomePage(
+        title: 'HealthFit Demo Home Page',
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+class _MyHomePage extends StatefulWidget {
+  _MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
 
   @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<_MyHomePage> {
+  String _platformVersion = 'Unknown';
+  int _stepCount = 0;
+  double _weight = 0.0;
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
@@ -41,15 +56,57 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void _updateStepCountState() {
+    // FIXME: replace with plugin methods
+    setState((){
+      if (_stepCount == 0) {
+        _stepCount = 10000;
+      } else {
+        _stepCount = 0;
+      }
+
+      if (_weight == 0.0) {
+        _weight = 65.7;
+      } else {
+        _weight = 0.0;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: new Center(
-          child: new Text('Running on: $_platformVersion\n'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(child: Text('Platform version: $_platformVersion')),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(child: Text('StepCount: $_stepCount')),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(child: Text('Weight: $_weight')),
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize:  MainAxisSize.max,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.refresh),
+        onPressed: () async {
+          _updateStepCountState();
+        },
       ),
     );
   }
