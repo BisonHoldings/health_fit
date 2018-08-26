6ef56f68d319d76f4f5922479bfa612d4ba89c73
neonankiti <yk.nanri@gmail.com>
2018-08-26 18:07:26 +0900

    make arguments as variants Steps are not the only datatype, so make it as variants since clients want to pass datatype.

--- android/src/main/kotlin/com/example/healthfit/HealthFitPlugin.kt
+++ android/src/main/kotlin/com/example/healthfit/HealthFitPlugin.kt
@@ -22,7 +22,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.Scopes
 import com.google.android.gms.common.api.Scope
 import com.google.android.gms.fitness.HistoryApi
+import com.google.android.gms.fitness.data.DataSource
 import java.util.*
+import com.google.android.gms.fitness.data.DataSource.TYPE_DERIVED
+import com.google.android.gms.fitness.data.Field
+import java.text.SimpleDateFormat
 
 
 class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, ActivityResultListener {
@@ -43,7 +47,7 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
     private var deferredResult: Result? = null
 
     override fun onMethodCall(call: MethodCall, result: Result): Unit {
-        Log.e("HFP: onMethodCall", "${call.method}")
+        Log.d("HFP: onMethodCall", "${call.method}")
         when (call.method) {
             "getPlatformVersion" -> {
             }
@@ -51,14 +55,7 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
             "requestPermission" -> requestPermission(call, result)
             "disable" -> disable(result)
             "getData" -> {
-                val cal = Calendar.getInstance()
-                val now = Date()
-                cal.setTime(now)
-                val endTime = cal.getTimeInMillis()
-                cal.add(Calendar.MONTH, -1)
-                val startTime = cal.getTimeInMillis()
-                getData(result, DataType.TYPE_STEP_COUNT_DELTA, startTime, endTime, TimeUnit.DAYS)
-                // TODO add getData method with correct arguments
+                getData(result, getDataType(call), getDataField(call), getStartAt(call), getEndAt(call), getTimeUnit(call))
             }
             else -> result.notImplemented()
         }
@@ -68,9 +65,9 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
         val healthFitOption = getHealthFitOptions(call)
         val optionsBuilder = FitnessOptions.builder()
         optionsBuilder.addDataType(healthFitOption.dataType, healthFitOption.permission)
-        Log.e("HFP: hasPermission option", "${healthFitOption.dataType}:${healthFitOption.permission}")
-        Log.e("HFP: hasPermission Account", "${GoogleSignIn.getLastSignedInAccount(activity)}")
-        Log.e("HFP: hasPermission", "${GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(activity), optionsBuilder.build())}")
+        Log.d("HFP: hasPermission option", "${healthFitOption.dataType}:${healthFitOption.permission}")
+        Log.d("HFP: hasPermission Account", "${GoogleSignIn.getLastSignedInAccount(activity)}")
+        Log.d("HFP: hasPermission", "${GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(activity), optionsBuilder.build())}")
         result.success(GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(activity), optionsBuilder.build()))
     }
 
@@ -79,9 +76,23 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
 
 
     private fun getDataType(call: MethodCall): DataType = when (call.argument<String>("dataType")) {
-        "com.google.step_count.delta" -> DataType.TYPE_STEP_COUNT_DELTA
+        "DataType.STEP" -> DataType.TYPE_STEP_COUNT_DELTA
         else -> throw IllegalArgumentException("${call.argument<String>("dataType")} is not allowed here.")
     }
+
+    private fun getDataField(call: MethodCall): Field = when (call.argument<String>("dataType")) {
+        "DataType.STEP" -> Field.FIELD_STEPS
+        else -> throw IllegalArgumentException("${call.argument<String>("dataType")} is not allowed here.")
+    }
+
+    private fun getTimeUnit(call: MethodCall): TimeUnit = when (call.argument<String>("timeUnit")) {
+        "TimeUnit.MILLISECONDS" -> TimeUnit.MILLISECONDS
+        "TimeUnit.HOURS" -> TimeUnit.HOURS
+        else -> throw IllegalArgumentException("${call.argument<String>("timeUnit")} is not allowed here.")
+    }
+
+    private fun getStartAt(call: MethodCall): Long = call.argument<Long>("startAt")
+    private fun getEndAt(call: MethodCall): Long = call.argument<Long>("endAt")
 
     private fun getPermission(call: MethodCall): Int = when (call.argument<Int>("permission")) {
         0 -> FitnessOptions.ACCESS_READ
@@ -114,14 +125,6 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
                 .requestEmail()
                 .build()
         activity.startActivityForResult(GoogleSignIn.getClient(activity, gso).signInIntent, GOOGLE_SIGN_IN_REQUEST_CODE)
-//                .addOnSuccessListener {
-//                    Log.e("HFP: attemptSignIn", "success")
-//                    callback(true)
-//                }
-//                .addOnFailureListener {
-//                    Log.e("HFP: attemptSignIn", "failure")
-//                    callback(false)
-//                }
     }
 
     private fun requestPermission(call: MethodCall, result: Result) {
@@ -149,20 +152,35 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
         }
     }
 
-    private fun getData(result: Result, dataType: DataType, startAt: Long, endAt: Long, bucket: TimeUnit) {
+    private fun getData(result: Result, dataType: DataType, field: Field, startAt: Long, endAt: Long, bucket: TimeUnit) {
+        Log.d("HFP: getData starTime", startAt.toString())
+        Log.d("HFP: getData endTime", endAt.toString())
         val dataRequest = createReadRequest(dataType, startAt, endAt, bucket)
         GoogleSignIn.getLastSignedInAccount(activity)?.let {
             Fitness.getHistoryClient(activity, it)
                     .readData(dataRequest)
+                    .addOnSuccessListener {
+                        Log.d("HFP: getData success", it.getDataSet(dataType).toString())
+                        val data = mutableListOf<String>()
+                        for (point in it.getDataSet(dataType).dataPoints) {
+                            // FIXME passing the object to the healthfit dart as object.
+                            val dateFormat = SimpleDateFormat("MM月dd日 HH時mm分ss秒")
+                            val dateString = dateFormat.format(Date(point.getTimestamp(bucket)))
+                            data.add("$dateString: ${point.getValue(field)}")
+                        }
+                        result.success(data.joinToString(separator = "\n"))
+                    }
+                    .addOnFailureListener {
+                        Log.e("HFP: getData failed", it.toString())
+                    }
                     .addOnCompleteListener {
-                        result.success("100")
                         // TODO add serializer for sending it to dart interface.
                     }
         }
         if (GoogleSignIn.getLastSignedInAccount(activity) == null) {
             attemptSignIn {
                 if (it) {
-                    getData(result, dataType, startAt, endAt, bucket)
+                    getData(result, dataType, field, startAt, endAt, bucket)
                 } else {
                     Log.e("HFP: getData", "failed to sign in")
                 }
@@ -173,17 +191,18 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
     private fun createReadRequest(dataType: DataType,
                                   startAt: Long,
                                   endAt: Long,
-                                  bucket: TimeUnit) = DataReadRequest.Builder()
-            .read(dataType)
-            // TODO making bucket time more flexible
-            .bucketByTime(1, bucket)
-            .setTimeRange(startAt, endAt, bucket)
-            .build()
+                                  bucket: TimeUnit): DataReadRequest {
+        return DataReadRequest.Builder()
+                .read(dataType)
+                .setTimeRange(startAt, endAt, bucket)
+                // TODO remove this later.
+                .setLimit(30)
+                .build()
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent): Boolean = when (resultCode) {
-    // TODO: put the concrete messages because requests and results have multiple status.
         Activity.RESULT_OK -> {
-            Log.e("HFP: onActivityResult", "requestCode:${requestCode} resultCode: ${resultCode}")
+            Log.d("HFP: onActivityResult", "requestCode:$requestCode resultCode: $resultCode")
             when (requestCode) {
                 GOOGLE_FIT_PERMISSIONS_REQUEST_CODE -> {
                     deferredResult?.success(true)
@@ -203,12 +222,13 @@ class HealthFitPlugin(private val activity: Activity) : MethodCallHandler, Activ
             }
         }
         else -> {
-            Log.e("HFP: onActivityResult", "requestCode:${requestCode} resultCode: ${resultCode}")
+            Log.d("HFP: onActivityResult", "requestCode:$requestCode resultCode: $resultCode")
             deferredResult?.success(false)
             deferredResult = null
             false
         }
     }
+
 }
 
 data class HealthFitOption(val dataType: DataType, val permission: Int)
