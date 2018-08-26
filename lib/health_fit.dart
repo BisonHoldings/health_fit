5f923fe8c93d9f5360d8f2eebab7c8d731c97b07
neonankiti <yk.nanri@gmail.com>
2018-08-26 18:14:21 +0900

    put arguments for lib interface

--- lib/health_fit.dart
+++ lib/health_fit.dart
@@ -3,21 +3,22 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class HealthFit {
-  static const MethodChannel _channel =
-      const MethodChannel('health_fit');
+  static const MethodChannel _channel = const MethodChannel('health_fit');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<bool> get hasPermission async {
-    final bool hasPermission = await _channel.invokeMethod('hasPermission');
+  static Future<bool> hasPermission(DataType type) async {
+    final bool hasPermission = await _channel.invokeMethod(
+        'hasPermission', {"dataType": type.toString(), "permission": 0});
     return hasPermission;
   }
 
-  static Future<bool> get requestPermission async {
-    final bool requestPermission = await _channel.invokeMethod('requestPermission');
+  static Future<bool> requestPermission(DataType type) async {
+    final bool requestPermission = await _channel.invokeMethod(
+        'requestPermission', {"dataType": type.toString(), "permission": 0});
     return requestPermission;
   }
 
@@ -27,9 +28,17 @@ class HealthFit {
   }
 
   // FIXME add serializer and return to clients(dart) to the mapped objects
-  static Future<String> get data async {
-    final String data = await _channel.invokeMethod('getData');
+  static Future<String> getData(DataType type, DateTime startAt, DateTime endAt,
+      TimeUnit timeUnit) async {
+    final String data = await _channel.invokeMethod('getData', {
+      "dataType": type.toString(),
+      "startAt": startAt.millisecondsSinceEpoch,
+      "endAt": endAt.millisecondsSinceEpoch,
+      "timeUnit": timeUnit.toString(),
+    });
     return data;
   }
-
 }
+
+enum DataType { STEP, WEIGHT }
+enum TimeUnit { MILLISECONDS, HOURS }
